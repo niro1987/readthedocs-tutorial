96ced236b81388cea48f2c723993d92997a19e32
Niels Perfors <niro1987@users.noreply.github.com>
2024-09-28 12:14:39 +0000

    rst only

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,3 @@
-import os
-
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
@@ -13,16 +11,12 @@ version = '0.1.0'
 
 # -- General configuration
 
-root_doc = 'index'
 extensions = [
-    'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.todo',
 ]
 
 intersphinx_mapping = {
@@ -36,27 +30,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-
-
-# myst settings
-source_suffix = ['.rst', '.md']
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
